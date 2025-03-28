32771621aa81fced86f0c74d96e8c2dc51afc648
SonuThapaMagar <sonumagar787@gmail.com>
2025-03-28 07:41:38 +0545

    Single landing page done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Header from '../header/Header'
-import Footer from '../footer/Footer'
-import Content from '../content/Content'
-
+import React from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import Content from "../content/Content";
 
 const Landing = () => {
   return (
-  <>
-    <Header/>
-    <Content/>
-    <Footer/>
-  </>
-  )
-}
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Content />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
